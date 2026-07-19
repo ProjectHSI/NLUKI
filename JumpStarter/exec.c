8eaa868d24f5a90dbf56bafe5004ab33d3628589
ProjectHSI <projecthsi@outlook.com>
2026-07-19 19:53:19 +1000

    add (& comment) dir list diagnostics

--- JumpStarter/exec.c
+++ JumpStarter/exec.c
@@ -1,6 +1,21 @@
 #pragma once
 
 #include <sys/mount.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+#define PERFORM_DIR_LIST_DIAGNOSTIC true
+
+void performListDiag(const char *loc) {
+	printf("[DIAG] reading dir...\n");
+	DIR *dir = opendir(loc);
+	struct dirent *currentReadDir = NULL;
+	while (currentReadDir = readdir(dir)) {
+		printf("%s\n", currentReadDir->d_name);
+	}
+	closedir(dir);
+	printf("[DIAG] done...\n");
+}
 
 void exec(int argc, char *argv[], char *envp[]) {
 	unlink("/bin/init");
@@ -25,6 +40,15 @@ void exec(int argc, char *argv[], char *envp[]) {
 		printf("Failed final chdir. %i\n", errno);
 		return;
 	}
+
+#if 0
+	if (PERFORM_DIR_LIST_DIAGNOSTIC) {
+		performListDiag("/");
+		performListDiag("/usr");
+		performListDiag("/usr/local");
+		performListDiag("/usr/local/bin");
+	}
+#endif
 
 	//argv[0] = "/sbin/init";
 	argv[0] = "/usr/local/bin/bash";
